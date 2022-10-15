3306ed1dd278cd8524797c6b0f29544a198a1a18
Holger Mayer <holgermayer@icloud.com>
2022-10-15 10:54:08 +0200

    Testfunctions - empty stubs

--- CoreDataDemoTests/Model/Views/Entities/ViewModels/CityViewModelTests.swift
+++ CoreDataDemoTests/Model/Views/Entities/ViewModels/CityViewModelTests.swift
@@ -6,30 +6,58 @@
 //
 
 import XCTest
+@testable import CoreDataDemo
 
 final class CityViewModelTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testInitWithCountry() throws {
+        XCTFail()
+    }
+    
+    func testInitWithCity() throws {
+        XCTFail()
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testIsValid_nameEmpty() throws {
+        XCTFail()
+
     }
 
+    func testIsValid_nameWhitespaces() throws {
+        XCTFail()
+
+    }
+    
+    func testIsValid_validName() throws {
+        XCTFail()
+
+    }
+
+    func testIsUpdating_newCityUnsaved() throws {
+        XCTFail()
+    }
+    
+    
+    func testIsUpdating_savedCity() throws {
+        XCTFail()
+    }
+    
+    func testUpdate_newCity() throws {
+        XCTFail()
+    }
+    
+    func testUpdate_existingCity() throws {
+        XCTFail()
+    }
+    
 }
+
+
--- CoreDataDemoTests/Model/Views/Entities/ViewModels/CountryViewModelTests.swift
+++ CoreDataDemoTests/Model/Views/Entities/ViewModels/CountryViewModelTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import CoreDataDemo
 
 final class CountryViewModelTests: XCTestCase {
 
@@ -17,19 +18,44 @@ final class CountryViewModelTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testInit() throws {
+        XCTFail()
+    }
+    
+    func testInitWithCountry() throws {
+        XCTFail()
+    }
+    
+
+    func testIsValid_nameEmpty() throws {
+        XCTFail()
+
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testIsValid_nameWhitespaces() throws {
+        XCTFail()
+
+    }
+    
+    func testIsValid_validName() throws {
+        XCTFail()
+
     }
 
+    func testIsUpdating_newCountryUnsaved() throws {
+        XCTFail()
+    }
+    
+    
+    func testIsUpdating_savedCountry() throws {
+        XCTFail()
+    }
+    
+    func testUpdate_newCountry() throws {
+        XCTFail()
+    }
+    
+    func testUpdate_existingCountry() throws {
+        XCTFail()
+    }
 }
